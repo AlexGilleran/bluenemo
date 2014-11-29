fc3d6beb01a039c7d949fc1ed8dd5d12cb3469f1
Alex Gilleran <alex.gilleran@gmail.com>
2014-11-29 16:24:55 +1100

    Changed map to display full list of fake GPS coords.

--- frontend/js/data/fusion-data.js
+++ frontend/js/data/fusion-data.js
@@ -1,7 +1,7 @@
 define(["flight"], function(flight) {
 	return flight.component(function() {
 		this.onDataRequested = function() {
-			$.get("https://www.googleapis.com/fusiontables/v2/query?sql=SELECT+*+FROM+1NFVpw27C4ki7se6AR3yOiyakAStUuiiC2xYYd8-B+LIMIT+10&key=AIzaSyBq6tmLr4DUJaXxVfHLwdoLv5UxomLv4Pw")
+			$.get("https://www.googleapis.com/fusiontables/v2/query?sql=SELECT+*+FROM+1Zxy_07rdpivaxnw_dYipgp6CNSWen17tUUHKKxH9&key=AIzaSyBq6tmLr4DUJaXxVfHLwdoLv5UxomLv4Pw")
 				.done(this.onDataReceived.bind(this))
 				.fail(this.onDataFailed.bind(this));
 		};
