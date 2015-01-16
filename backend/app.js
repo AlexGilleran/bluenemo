c2320835f47110a6e2292dd3bbcdbda02af262ed
Thomas Dane <tjdane@gmail.com>
2015-01-17 10:26:29 +1100

    add node files to separate backend folder

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ var http = require('http'),
 
 
 
-fs.readFile('./index.html', function (err, html) {
+fs.readFile('../frontend/index.html', function (err, html) {
     if (err) {
         throw err; 
     }       
